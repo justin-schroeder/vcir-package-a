ea2e245334fea9dcb3dd617070e02cf26456ce95
Justin Schroeder <justin@formkit.com>
2024-01-15 16:52:28 -0500

    chore: initial commit

---
+++ dist/index.dev.mjs
@@ -0,0 +1,9 @@
+// src/index.ts
+var i = 0;
+console.log('vcir-package DEV MODE')
+function count() {
+  return i++;
+}
+export {
+  count
+};
---
+++ dist/index.mjs
@@ -0,0 +1,8 @@
+// src/index.ts
+var i = 0;
+function count() {
+  return i++;
+}
+export {
+  count
+};
